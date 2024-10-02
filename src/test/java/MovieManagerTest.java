7dda2777037fe78e6f2234f09ba35b9d206d0d15
Yuliya.S <yuliyamarketplace@yandex.ru>
2024-10-02 10:35:57 +0400

    find the error

---
+++ src/test/java/MovieManagerTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+
+    @Test
+    public void test() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void moviesReverseOrder() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
+
+
+
+
