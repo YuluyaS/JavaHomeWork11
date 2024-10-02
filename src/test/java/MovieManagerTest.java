3049756f3a781e348020381cf31a7d15f6572fb1
Yuliya.S <yuliyamarketplace@yandex.ru>
2024-10-02 15:24:03 +0400

    finish the tests

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -10,8 +10,10 @@ public class MovieManagerTest {
         manager.addMovie("Film 1");
         manager.addMovie("Film 2");
         manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
 
-        String[] expected = {"Film 1", "Film 2", "Film 3"};
+        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5"};
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -23,8 +25,11 @@ public class MovieManagerTest {
         manager.addMovie("Film 1");
         manager.addMovie("Film 2");
         manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
 
-        String[] expected = {"Film 3", "Film 2", "Film 1"};
+
+        String[] expected = { "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
